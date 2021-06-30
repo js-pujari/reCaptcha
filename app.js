e1588685d6738e76b1a06acb8adba002420a41c7
pundalik.pujari <pundalik.pujari@mediassist.in>
2021-06-30 18:53:13 +0530

    port

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('Server started');
+    console.log('Server started on port ', port);
 })
 app.use(express.json())
 app.use('/', defRouter);
